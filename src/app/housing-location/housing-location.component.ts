bcb5189100a6b7d4abd23f766f2a69b0af71d248
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-07-16 16:49:53 +0100

    tutorial p5 Add an input parameter complete

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {HousingLocation} from '../housinglocation';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
-export class HousingLocationComponent {
+/*
+https://angular.dev/tutorials/first-app/05-inputs
+You have to add the ! because the input is expecting the value to be passed. In this case, there is no default value. 
+In our example application case we know that the value will be passed in - this is by design. The exclamation point is 
+called the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or 
+undefined.
+ */
 
+
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
