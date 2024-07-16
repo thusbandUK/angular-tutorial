9c7aab7c00c7134acc40ef9940ec22c40034cbf9
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-07-16 16:42:08 +0100

    tutorial p3 housing location component complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+//import {CommonModule} from '@angular/common';
+
+/*
+this was in imports CommonModule. v frustrating, basically their app shows the commonmodule being imported (see commented out
+line above) but including common module in the imports crashes the app 
+*/
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
+  imports: [HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
