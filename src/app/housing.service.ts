02f596ca8739f6a88ae5c258cd50b93ff7e847b8
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-07-17 07:35:31 +0100

    tutorial p12 Adding a form complete

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -118,6 +118,12 @@ export class HousingService {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
+
   constructor() {
 
     
